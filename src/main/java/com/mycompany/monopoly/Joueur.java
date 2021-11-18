79909ff70769ce50b3be38fc234680982563cc9d
mariusBRM <74615809+mariusBRM@users.noreply.github.com>
2021-11-18 09:48:33 +0100

    constructeur

--- src/main/java/com/mycompany/monopoly/Joueur.java
+++ src/main/java/com/mycompany/monopoly/Joueur.java
@@ -5,6 +5,12 @@ public class Joueur{
     private double fortune;
     private int position;
     private Plateau plateau;
+    
 
+    public void Joueur(String nom, Plateau plateau){
+        this.nom = nom;
+        this.plateau = plateau;
+    }
+    
     
 }
