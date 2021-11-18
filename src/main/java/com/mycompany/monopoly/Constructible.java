628df6089ac31639ffde33fd93647a20846bdd0d
alan <alanroussel@free.fr>
2021-11-18 09:09:40 +0100

    squelette classe constructible

--- src/main/java/com/mycompany/monopoly/Constructible.java
+++ src/main/java/com/mycompany/monopoly/Constructible.java
@@ -1,5 +1,18 @@
 package com.mycompany.monopoly;
 
 public class Constructible extends Achetable{
-    
+    private int nbMaisons;
+    private int nbHotel;
+    static double prixMaison;
+    static double prixHotel;
+
+    public double loyer(){
+
+    }
+    public void ajoutMaison(){
+
+    }
+    public void ajoutHotel(){
+        
+    }
 }
