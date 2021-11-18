52bb82149763e758d6d30fb794d0ec984438298d
alan <alanroussel@free.fr>
2021-11-18 09:52:14 +0100

    tour de jeu

--- src/main/java/com/mycompany/monopoly/Joueur.java
+++ src/main/java/com/mycompany/monopoly/Joueur.java
@@ -3,8 +3,20 @@ package com.mycompany.monopoly;
 public class Joueur{
     private String nom;
     private double fortune;
-    private int position;
+    private Case position;
     private Plateau plateau;
 
-    
+
+    public void tourDeJeu() throws NoMoreMoneyException{
+        // lancer le dé
+        int de = lancerDe();
+        position = plateau.avancer(this.position, de);
+        position.utiliser(this);
+        
+    }
+
+    public String toString(){
+        String res = "vous êtes le joueur " + this.nom + ", vous possedez : " + this.fortune + "€" + " et vous êtes en " + position.getName();
+        return res;
+    }    
 }
