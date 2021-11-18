bc67cfc6fab9e1eeec6756f23900f00618e3d4ae
alan <alanroussel@free.fr>
2021-11-18 09:14:37 +0100

    finish

--- src/main/java/com/mycompany/monopoly/Constructible.java
+++ src/main/java/com/mycompany/monopoly/Constructible.java
@@ -1,18 +1,25 @@
 package com.mycompany.monopoly;
 
 public class Constructible extends Achetable{
-    private int nbMaisons;
-    private int nbHotel;
-    static double prixMaison;
-    static double prixHotel;
+    private int nbMaison;
+    private int nbHotel; 
+    static double prixMaison = 100 ;
+    static double prixHotel = 1000 ;
 
+    // où fait on le lien avec l'utilisateur ? 
+    public Constructible(){
+        this.nbMaison = 0;
+        this.nbHotel = 0;
+        // autre chose ? 
+    }
     public double loyer(){
-
+        double res = this.nbMaison*prixMaison + this.nbHotel*prisHotel
+        return res;
     }
     public void ajoutMaison(){
-
+        this.nbMaison += 1;
     }
     public void ajoutHotel(){
-        
+        this.nbHotel += 1;
     }
 }
