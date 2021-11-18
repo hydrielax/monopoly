53bfb1927141291d2e3f856077cec084ad0f0249
youness* <oumhamed.younes.mpsi@gmail.com>
2021-11-18 10:30:18 +0100

    Achetable

--- src/main/java/com/mycompany/monopoly/Achetable.java
+++ src/main/java/com/mycompany/monopoly/Achetable.java
@@ -12,5 +12,39 @@ package com.mycompany.monopoly;
 public abstract class Achetable extends Case {
     private Joueur proprietaire;
     private int prix;
+
+    public Achetable(Joueur proprietaire, int prix, String nom) {
+        super(nom);
+        this.proprietaire = proprietaire;
+        this.prix = prix;
+    }
+
+    public Achetable(int prix, String nom) {
+        super(nom);
+        this.prix = prix;
+    }
+    
+    public Achetable(String nom){
+        super(nom);
+    }
+    
+    public int getPrix() {
+        return prix;
+    }
+
+    public void setPrix(int prix) {
+        this.prix = prix;
+    }
+
+    public Joueur getProprietaire() {
+        return proprietaire;
+    }
+
+    public void setProprietaire(Joueur proprietaire) {
+        this.proprietaire = proprietaire;
+    }
+
+
+    
     
 }
