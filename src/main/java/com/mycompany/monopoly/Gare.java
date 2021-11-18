2c1ce9cdb99ba6cf71af1c5739426e2aeeaa90fb
jbhmn <jb.hammann@gmail.com>
2021-11-18 09:04:30 +0100

    création gare et joueur

---
+++ src/main/java/com/mycompany/monopoly/Gare.java
@@ -0,0 +1,5 @@
+
+
+public class Gare extends Achetable{
+    
+}
---
+++ src/main/java/com/mycompany/monopoly/Joueur.java
@@ -0,0 +1,8 @@
+public class Joueur{
+    private String nom;
+    private double fortune;
+    private int position;
+    private Plateau plateau;
+
+    
+}
